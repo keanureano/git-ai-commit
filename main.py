78320a9debbfae57b7dad103cab7dd1fed7ef458
Keanu Reaño <reanokeanu@gmail.com>
2023-08-04 14:50:28 +0800

    `Update HUG Chatbot Commit Message Script`

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
+from hugchat import hugchat
+from hugchat.login import Login
+from dotenv import load_dotenv
 import subprocess
 
 
 def main():
     staged_diff_logs = git_diff()
+    commit_message = generate_commit_message(staged_diff_logs)
 
 
 def git_diff():
@@ -17,3 +22,38 @@ def git_diff():
     else:
         print(result.stderr)
         return None
+
+
+def generate_commit_message(staged_diff_logs):
+    while True:
+        load_dotenv()
+        EMAIL = os.getenv("EMAIL")
+        PASSWORD = os.getenv("PASSWORD")
+        hugging_face_login = Login(EMAIL, PASSWORD)
+
+        cookies = hugging_face_login.login()
+        chatbot = hugchat.ChatBot(cookies=cookies.get_dict())
+
+        prompt = f"""
+        Task: Generate a concise commit message based on the given Git diff logs.
+        Do not include any additional information or explanations. Only the command should be given.
+
+        Git diff logs:
+        ```
+        {staged_diff_logs}
+        ```
+        """
+
+        commit_message = chatbot.chat(prompt)
+        print("\033[92m" + commit_message + "\033[0m")
+
+        response = input("Confirm commit? [Y]es/[R]etry: ")
+
+        if response == "y":
+            return commit_message
+        else:
+            continue
+
+
+if __name__ == "__main__":
+    main()
